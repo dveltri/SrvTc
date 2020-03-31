a3aa35cf789e7ceb73149bcda7a4bade695c0a1e
Daniel Veltri <dveltri@gmail.com>
2020-03-31 11:34:13 -0300

    update entrypoint

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
-/opt/tomcat/latest/bin/./startup.sh
+export TOMCAT="/opt/tomcat/latest/webapps/"
 cd /opt/tomcat/latest/webapps/
 git checkout anibal
 git pull
+cd ROOT/lp3
+git checkout anibal
+git pull
+/opt/tomcat/latest/bin/./startup.sh
 #cd /usr/lib/postgresql/12/bin
 cd /
 chmod 666 /tmp/logs.log
