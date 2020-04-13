b4f705a75993e1b22187aef7278202ad8ef567cb
Daniel Veltri <dveltri@gmail.com>
2020-04-12 22:44:18 -0300

    add argument for disabled router

--- PDgvDrv/SrvPdgv1.sh
+++ PDgvDrv/SrvPdgv1.sh
@@ -5,6 +5,6 @@ while [ 1==1 ]
 do
 fecha=$(date +%Y%m%d%H%M%S)
 echo "Start Pdgv drv1 ${fecha}" >> /tmp/drvlogs.txt
-java SrvPdgvX drv1 254 2024 5 1 1
+java SrvPdgvX drv1 254 2024 5 1 1 0
 sleep 1
 done
